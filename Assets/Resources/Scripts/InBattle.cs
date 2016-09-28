9c974bc8874a75773b91b8e255042fe759139b21
Eduhlira <eduardu.silva@hotmail.com>
2016-09-28 18:19:43 -0400

    first commit

---
+++ Assets/Resources/Scripts/InBattle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class InBattle : MonoBehaviour {
+
+
+	public Image fighter, fighter2, fighter3;
+	public GameObject lutador1, lutador2, lutador3;
+
+	//public string nameImage;
+
+	// Use this for initialization
+	void Start () {
+		
+		fighter3.sprite = Resources.Load<Sprite> ("Sprites/"+PlayerPrefs.GetString("Lutador3"))as Sprite;
+		lutador3 = Resources.Load<GameObject> ("Prefabs/"+PlayerPrefs.GetString("Lutador3"))as GameObject;
+	
+		//fighter2.sprite = Resources.Load<Sprite> ("Sprites/"+PlayerPrefs.GetString("Lutador3"))as Sprite;
+		//fighter.sprite = Resources.Load<Sprite> ("Sprites/"+PlayerPrefs.GetString("Lutador3"))as Sprite;
+
+		//lutador3 = Resources.Load<GameObject> ("Prefabs/"+nameImage)as GameObject;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if( Input.GetKeyDown(KeyCode.X)){
+			//lutador3.SetActive(true);
+
+			GameObject Fighter=Instantiate(lutador3);
+			Fighter.transform.SetParent(gameObject.transform);
+		}
+	}
+
+
+}
---
+++ Assets/Resources/Scripts/ShowElement.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowElement : MonoBehaviour {
+
+	public GameObject elemento;
+	public string K, L, M, N, O, P, Q,aux_string;
+	public Text nEletrons;
+	public static int eletrons;
+	public static int Bromo, qtdbromo;
+
+	public GameObject errorMensage;
+
+	// Use this for initialization
+	void Start () {
+
+
+		eletrons= PlayerPrefs.GetInt("QtdEletrons");
+		nEletrons.text = (PlayerPrefs.GetInt("QtdEletrons").ToString());//recebe a quantidade de eletrons armazenados e mostraa;
+		Bromo = PlayerPrefs.GetInt("Bromo");
+		qtdbromo = PlayerPrefs.GetInt("qtdBromo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( Input.GetKeyDown(KeyCode.X)){
+			aumentarEletrons();
+		}
+	}
+
+	public void ShowElements(string newText){
+		this.aux_string= newText;
+	}
+	public void SaveTo(string ToSave){
+		if(ToSave == "K"){
+			this.K= aux_string;
+		}
+		if(ToSave == "L"){
+			this.L= aux_string;
+		}
+		if(ToSave == "M"){
+			this.M= aux_string;
+		}
+		if(ToSave == "N"){
+			this.N= aux_string;
+		}
+		if(ToSave == "O"){
+			this.O= aux_string;
+		}
+		if(ToSave == "P"){
+			this.P= aux_string;
+		}
+		if(ToSave == "Q"){
+			this.Q= aux_string;
+		}
+	}
+	public void Checkword(){
+		
+		//Cria o Bromo
+		if (K == "2" && L == "8" && M == "18" && N == "8" && O == "0" && P == "0" && Q == "0" && PlayerPrefs.GetInt("QtdEletrons")>= 35){
+			elemento.SetActive(true);
+			ShowElement.eletrons=ShowElement.eletrons - 36;
+			PlayerPrefs.SetInt("QtdEletrons", ShowElement.eletrons); 
+			nEletrons.text = (PlayerPrefs.GetInt("QtdEletrons").ToString());//recebe a quantidade de eletrons armazenados e mostraa;
+			ShowElement.Bromo = 1;
+			ShowElement.qtdbromo = 1;
+			PlayerPrefs.SetInt("Bromo", ShowElement.Bromo);
+
+		}else{
+			elemento.SetActive(false);
+			errorMensage.SetActive(true);
+		}
+  }
+	void aumentarEletrons(){
+	
+		ShowElement.eletrons = ShowElement.eletrons+1;
+		PlayerPrefs.SetInt("QtdEletrons",ShowElement.eletrons); //onde armazeno a quantidade de eletrons do ususario;
+		nEletrons.text = (PlayerPrefs.GetInt("QtdEletrons").ToString());//recebe a quantidade de eletrons armazenados e mostraa;
+
+	}
+}
