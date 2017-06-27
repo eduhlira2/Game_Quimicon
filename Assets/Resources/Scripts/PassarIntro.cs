e39b276dfe28a9546bca18fc3d6af5e1f86d4aca
eduhlira2 <eduardu.silva@hotmail.com>
2017-06-27 00:11:36 -0400

    introdução

---
+++ Assets/Resources/Scripts/PassarIntro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassarIntro : MonoBehaviour {
+
+	public GameObject ControleAnim;
+	public int numeroIntro = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void passarAnim(){
+		numeroIntro++;
+			ControleAnim.GetComponent<Animator> ().Play ("Introduçao_"+numeroIntro);
+			
+	}
+}
